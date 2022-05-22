4163bcda0994bfd8e37e2b4f7eff98a6af22efe5
Robin Engström <robin@snabbfot.org>
2022-05-22 20:18:03 +0200

    Some cleaning

--- pages/api/myrstigen.js
+++ pages/api/myrstigen.js
@@ -1,27 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from 'fs';
 
-// export default function handler(req, res) {
-//   // try {
-//   //   const data = fs.readFileSync('../../JSONDATA/', 'utf8');
-//   //   console.log(data);
-//   // } catch (err) {
-//   //   console.error(err);
-//   // }
-//   console.log(req.params)
-//   res.status(200).json({ name: 'John Doe' })
-// }
-// import { NextApiRequest, NextApiResponse } from "next";
-
 export default async (request, response) => {
-
-  console.log(request.query.year);
   try {
     const data = fs.readFileSync('./pages/api/JSONDATA/Myr_avd_' + request.query.year + '.json', 'utf8');
-    console.log(data);
     return response.status(200).json(JSON.parse(data));
   } catch (err) {
     return response.status(500).json({ ...err});
   }
-  // do nothing fancy and simply return a string concatenation
 };
