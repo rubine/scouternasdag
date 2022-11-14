6144b4934193a87fa7f539428d394cb3dc178e86
Debian <bitnami@ip-172-26-5-97.eu-north-1.compute.internal>
2022-11-14 08:35:25 +0000

    fix https2

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
-      <link rel="canonical" href={`http://ssd.snabbfot.org${router.asPath}`} />
+      <link rel="canonical" href={`https://ssd.snabbfot.org${router.asPath}`} />
     </Head>
     <Component {...pageProps} />
   </>
